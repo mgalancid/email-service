61d6c22f4d63eba240856447b75300885f535dff
Max <53160586+mgalancid@users.noreply.github.com>
2025-01-27 19:16:52 -0300

    Removed EmailModel

--- src/main/java/com/mindhub/email_service/model/Email.java
+++
@@ -1,4 +0,0 @@
-package com.mindhub.email_service.model;
-
-public class Email {
-}
--- src/main/java/com/mindhub/email_service/services/impl/EmailServiceImpl.java
+++ src/main/java/com/mindhub/email_service/services/impl/EmailServiceImpl.java
@@ -7,9 +7,11 @@ import org.springframework.core.io.FileSystemResource;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
 
 import java.io.File;
 
+@Service
 public class EmailServiceImpl implements EmailService {
     @Autowired
     private JavaMailSender mailSender;
