9eb26954fb44a6a51fb8ecdb9adb7c762df91b3b
Max <53160586+mgalancid@users.noreply.github.com>
2025-01-30 23:48:08 -0300

    Added OrderToEmailDTO

---
+++ src/main/java/com/mindhub/email_service/dtos/OrderToEmailDTO.java
@@ -0,0 +1,34 @@
+package com.mindhub.email_service.dtos;
+
+import java.util.List;
+
+public class OrderToEmailDTO {
+    private Long orderId;
+    private String userEmail;
+    private String status;
+    private List<OrderItemDTO> products;
+
+    public OrderToEmailDTO(Long orderId, String userEmail, String status, List<OrderItemDTO> products) {
+        this.orderId = orderId;
+        this.userEmail = userEmail;
+        this.status = status;
+        this.products = products;
+    }
+
+    public Long getOrderId() {
+        return orderId;
+    }
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public List<OrderItemDTO> getProducts() {
+        return products;
+    }
+}
+
